e9ee5b6388eb7a3f1b9ac92e9971acf2dd2e38dd
Rahul Saklecha <rahulsaklecha101@gmail.com>
2021-07-24 03:33:40 +0530

    project skeleton

---
+++ src/test/java/com/lunchbox/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunchbox.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
